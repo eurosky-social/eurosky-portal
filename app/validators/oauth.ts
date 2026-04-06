4e1d77e56f709df6fc4e333410b8d2cda06cbcdf
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2026-04-06 22:53:04 +0200

    Allow login with input that is not quiet a handle

--- app/validators/oauth.ts
+++ app/validators/oauth.ts
@@ -2,7 +2,22 @@ import vine from '@vinejs/vine'
 
 export const loginRequestValidator = vine.create({
   input: vine.unionOfTypes([
-    vine.atproto.handle(),
+    // We allow login with @username.eurosky.social, username@eurosky.social or
+    // @username@eurosky.social. These aren't standard handle formats, but what
+    // we've seen entered during user testing.
+    vine.atproto.handle().parse((value) => {
+      if (typeof value === 'string') {
+        let newValue = value
+        if (value.startsWith('@')) {
+          newValue = value.slice(1)
+        }
+        if (newValue.includes('@')) {
+          newValue = newValue.replace('@', '.')
+        }
+        return newValue
+      }
+      return value
+    }),
     vine.atproto.did(),
     vine.atproto.service(),
     vine.atproto.handleUsername(),
