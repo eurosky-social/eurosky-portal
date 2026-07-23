041c082cf7c1b34eceb552343e6954abe83f3814
Titus Wormer <tituswormer@gmail.com>
2026-07-23 16:09:23 +0200

    Add feedback link to beta banner
    per @renderghost

--- inertia/components/BetaWarning.tsx
+++ inertia/components/BetaWarning.tsx
@@ -1,12 +1,16 @@
+const feedbackUrl = 'https://userinput.app/#/s/did:plc:ooensn4mr5mhznzypvxelfa3/3mr5gmbhteg2p'
+
 export default function BetaWarning() {
   return (
     <div className="bg-brand py-2 px-3 text-center text-black/80 font-semibold">
       Eurosky Portal is currently in beta.{' '}
       <a
-        href="https://eurosky.tech/blog/eurosky-portal-beta/"
+        href={feedbackUrl}
+        rel="noopener noreferrer"
+        target="_blank"
         className="underline hover:text-black/60"
       >
-        Learn more
+        Give feedback
       </a>
       .
     </div>
